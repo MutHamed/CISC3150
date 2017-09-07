d9da2fcf4033a9c4df580336f1315a32be54500f
Motez <foreverproud@hotmail.com>
2017-09-06 23:00:50 -0400

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		double cen1 = 0.0, cen2 = 0.0;
+		double rad1 = 0.0, rad2 = 0.0;
+		
+		System.out.print("Enter Centers and radiuses for 2 circles: ");
+		
+		// Read input from user for centers
+		cen1 = input.nextDouble();
+		cen2 = input.nextDouble();
+		
+		// Read input from user for radiuses
+		rad1 = input.nextDouble();
+		rad2 = input.nextDouble();
+		
+		/*
+		 code to determine if circles overlap, separate, touch
+		 or within one another
+		 
+		 
+		 if (){
+		 }
+		 
+		 Not sure how to do this part, yet.
+		 */
+		
+
+	}
+
+}
